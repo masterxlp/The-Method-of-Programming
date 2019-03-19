12969228f296948dbf936e75ad5da8ed714b862b
masterxlp <masterxlp@163.com>
2019-03-19 22:34:26 +0800

    update 算法分析 and 举一反三_代码 file

--- string_permutation_all/举一反三_代码.py
+++ string_permutation_all/举一反三_代码.py
@@ -1,5 +1,23 @@
-def CalcAllPermutation(str_list, num, str_len):
+def CalcAllPermutation(str_list, lst, num, str_len):
 
-    if num == str_len-1:
-        print(''.join(str_list))
+    if num == str_len:
+        print(''.join(lst))
     else:
+        for i in range(str_len):
+            lst.append(str_list[i])
+            CalcAllPermutation(str_list, lst, num+1, str_len)
+            lst.pop()
+
+
+def main(string):
+
+    # Transfer string to list
+    string_list = list(string)
+    lst = list()
+    num = 0
+    string_len = len(string_list)
+    CalcAllPermutation(string_list, lst, num, string_len)
+
+
+if __name__ == "__main__":
+    main('abc')
