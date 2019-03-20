68a9a352c489b5f8a1927dafb64571fe223efece
masterxlp <masterxlp@163.com>
2019-03-20 11:31:33 +0800

    update 递归实现之过程分析 and 举一反三 file

--- string_permutation_all/举一反三_代码.py
+++ string_permutation_all/举一反三_代码.py
@@ -19,5 +19,24 @@ def main(string):
     CalcAllPermutation(string_list, lst, num, string_len)
 
 
+def CalcAllCombination(str_list, lst, num):
+
+    if len(lst) != 0:
+        print(''.join(lst))
+
+    for i in range(num, len(str_list)):
+        lst.append(str_list[i])
+        CalcAllCombination(str_list, lst, i+1)
+        lst.pop()
+
+
+def main2(string):
+
+    # Transfer string to list
+    string_list = list(string)
+    lst = list()
+    num = 0
+    CalcAllCombination(string_list, lst, num)
+
 if __name__ == "__main__":
-    main('abc')
+    main2('abc')
